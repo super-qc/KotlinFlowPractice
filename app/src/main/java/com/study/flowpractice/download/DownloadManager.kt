160d8da0fdd26cb582739ded630ccaa2916e0dd0
269223361@qq.com <269223361@qq.com>
2022-04-27 19:18:34 +0800

    完成文件下载功能

--- app/src/main/java/com/study/flowpractice/download/DownloadManager.kt
+++ app/src/main/java/com/study/flowpractice/download/DownloadManager.kt
@@ -1,15 +1,49 @@
 package com.study.flowpractice.download
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
+import okhttp3.OkHttpClient
+import okhttp3.Request
 import java.io.File
+import com.study.flowpractice.utils.copyTo
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.flowOn
+import okhttp3.Dispatcher
+import java.io.IOException
 
 object DownloadManager {
     fun download(url:String,file: File): Flow<DownloadStatus> {
 
         return flow{
+            var request= Request.Builder().url(url).get().build()
+            val response= OkHttpClient.Builder().build().newCall(request).execute()
+            if(response.isSuccessful){
+                response.body()!!.let {body->
+                    val total = body.contentLength()
+                    // 文件读写
+                    file.outputStream().use { output->
+                        val input = body.byteStream()
+                        var emittedProcess = 0L
+                        input.copyTo(output){ bytesCopied->
+                            val progress = bytesCopied*100/total
+                            if(progress-emittedProcess>5){
+                                kotlinx.coroutines.delay(100)
+                                emit(DownloadStatus.Progress(progress.toInt()))
+                                emittedProcess = progress
+                            }
+                        }
+                    }
 
-        }
+                }
+                emit(DownloadStatus.Done(file))
+            }else{
+                throw IOException(response.toString())
+            }
+        }.catch {
+            file.delete()
+            emit(DownloadStatus.Error(it))
+        }.flowOn(Dispatchers.IO)
     }
 
 }
--- app/src/main/java/com/study/flowpractice/fragment/DownloadFragment.kt
+++ app/src/main/java/com/study/flowpractice/fragment/DownloadFragment.kt
@@ -1,17 +1,25 @@
 package com.study.flowpractice.fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.study.flowpractice.R
+import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
 import com.study.flowpractice.databinding.FragmentDownloadBinding
-import com.study.flowpractice.databinding.FragmentHomeBinding
+import com.study.flowpractice.download.DownloadManager
+import com.study.flowpractice.download.DownloadStatus
+import kotlinx.coroutines.flow.collect
+import java.io.File
 
-
+/**
+ * 从网络上下载图片，保存到 Android/data/包名/files/ 目录下
+ */
 class DownloadFragment : Fragment() {
 
+    val URL="https://qniu.puncheers.com/1565148249957"
 
     private val mBinding: FragmentDownloadBinding by lazy {
         FragmentDownloadBinding.inflate(layoutInflater)
@@ -23,6 +31,40 @@ class DownloadFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return mBinding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        lifecycleScope.launchWhenCreated {
+            context?.apply {
+                val file = File(getExternalFilesDir(null)?.path,"logo.jpg")
+
+                DownloadManager.download(URL,file).collect {status->
+                    when(status){
+                        is DownloadStatus.Progress->{
+                            mBinding.apply {
+                                progressBar.progress=status.value
+                                tvProgress.text="${status.value}%"
+                            }
+                        }
+                        is DownloadStatus.Error->{
+                            Toast.makeText(context,"下载错误",Toast.LENGTH_SHORT).show()
+                        }
+                        is DownloadStatus.Done -> {
+                            mBinding.apply {
+                                progressBar.progress=100
+                                tvProgress.text="100%"
+                            }
+                            Toast.makeText(context,"下载完成",Toast.LENGTH_SHORT).show()
+                        }
+                        else->{
+                            Log.d(this.javaClass.name,"下载失败")
+                        }
+                    }
+                }
+            }
+        }
     }
 
 
